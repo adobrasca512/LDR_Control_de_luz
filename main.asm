74dfc75aef41af7015fe7d4ab39111c1278e4284
Adilem Dobras Castillo <59998402+adobrasca512@users.noreply.github.com>
2021-06-16 02:07:27 +0100

    MEJORADO

--- main.asm
+++ main.asm
@@ -1,4 +1,8 @@
  ; finalizado
+.dseg
+valor: .db 1
+
+.cseg 
 ldi r16,0xff
 out ddrd,r16
 rcall config_adc
@@ -17,42 +21,45 @@ LDS R16,ADCH
 
 ;aqui viene la parte que es mi teoria:
 ;aqui formule la teoria de si tenia que usar pwm pero no lo use
-; ¿Sera que la analogica digital me dara el porcentaje en si? si me da una señal analogica..interpretada en digital...seria lo logico
+; Â¿Sera que la analogica digital me dara el porcentaje en si? si me da una seÃ±al analogica..interpretada en digital...seria lo logico
 ;que el adc me diera una aproximacion...sera que uso el pwm?
 ;CALCULOS DEL PORCENTAJE
 ;El 100% es 255 que son los bits que recibo en la parte alta
 ;viendo el 33% de su maxima capacidad que es 255=84.15
 ;viendo el 66% de su maxima capacidad que es 255=168.3
 ;comparo con 33%
-;mi ldr esta semi dañado porque el 66% lo ve como de un 100% de luz q le paso desde mi linternita 
+;mi ldr esta semi daÃ±ado porque el 66% lo ve como de un 100% de luz q le paso desde mi linternita 
 ;o quizas sea el codigo lo cual espero q no sea asi :(
-cpi r16,33
-;si es menor que el 33 encendemos todas
-brlo encender
-push r16
-rjmp encender_cuatro
-pop r16
 
-rjmp seguir
-encender: 
-ldi r20,0xff
-out portd,r20
- seguir:
+sts valor,r16
+cpi r16,84
+;si es menor que el 33 encendemos todas
+brlo encendertodas
+brsh encendercuatro; si es mayor encendemos cuatro
+rjmp fin
+encendertodas:
+push r20
+ser r20
+out portb,r20
+pop r20
+rjmp fin
+encendercuatro:
+push r20
+push r21
+lds r20,valor
+cpi r20,168
+brlo continuar
+rjmp fin
+continuar:
+ldi r21,0b00001111
+out portb,r21
+pop r21
+pop r20
+
+
+
+fin:
  rjmp loopGeneral
- encender_cuatro:
- push YL
- push YH
- IN YL, SPL
- IN YH,SPH
- ldd r19,Y+5
- ;si es menor que el 66% entonces prendemos 4
- cpi r19,0b10101000
- brlo continuar
- rjmp seguir
- continuar:
- ldi r20,0b00001111
-out portd,r20
- rjmp seguir
 
 config_adc:
 push r16
